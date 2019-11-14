01fdbe57776b43c876a54e54b583fa4a6fa88969
Jan Gerber <jan-456@t-online.de>
2019-11-14 23:55:08 +0100

    Threads added

--- __init__.py
+++ __init__.py
@@ -1,22 +1,47 @@
 import json
+import threading
 
 from flask import Flask
 
+from industrial_inf_assigment.orchestrator_input import OrchestratorInput
 from industrial_inf_assigment.orchestrator_rpi import Orchestrator
+from industrial_inf_assigment.orchestrator_status import OrchestratorStatus
 from industrial_inf_assigment.subsciber import Subscriber
 from industrial_inf_assigment.workstation import Workstation
 
+# Workstations
 w9 = Workstation("http://192.168.2", None)
 
+# Subscribers
 sub = Subscriber("http://192.168.0.108:5000")
 sub.subscribeToPenChangeEnd("http://192.168.2.1", "/rest/events/PenChangeEnd/info")
 sub.subscribeToPenChangeStart("http://192.168.2.1", "/rest/events/PenChangeStart/info")
 sub.subscribeToDrawingStart("http://192.168.2.1", "/rest/events/DrawStartExecution/info")
 sub.subscribeToDrawingEnd("http://192.168.2.1", "/rest/events/DrawEndExecution/info")
 
-orchestrator = Orchestrator()
+# Orchestration
+orchestratorStatus = OrchestratorStatus()
+orchestrator = Orchestrator(orchestratorStatus)
+orchestratorInput = OrchestratorInput(orchestrator)
 
 app = Flask(__name__)
+
+
+def runFlaskApp():
+    if __name__ == '__main__':
+        app.run("0.0.0.0")
+
+
+threadOrchestration = threading.Thread(target=orchestrator.runOrchestation, args=())
+threadOrchestrationInput = threading.Thread(target=orchestratorInput.startListening, args=())
+threadOrchestrationStatus = threading.Thread(target=orchestratorStatus.blink, args=())
+threadFlask = threading.Thread(target=runFlaskApp, args=())
+
+threadOrchestration.start()
+threadOrchestrationStatus.start()
+threadFlask.start()
+threadOrchestrationInput.start()
+
 
 
 @app.route('/rest/events/PenChangeEnd/info', methods=['POST'])
@@ -49,7 +74,3 @@ def drawingEndEvent():
     cnvMsg = {}
     cnvMsg_str = json.dumps(cnvMsg)
     return cnvMsg_str
-
-
-if __name__ == '__main__':
-    app.run("0.0.0.0")
--- industrial_inf_assigment/orchestrator_rpi.py
+++ industrial_inf_assigment/orchestrator_rpi.py
@@ -1,14 +1,24 @@
+import time
 import uuid
 
+from industrial_inf_assigment.orchestrator_status import OrchestratorStatus
 from industrial_inf_assigment.phone import Phone
+from industrial_inf_assigment.status_code import StatusCode
 
 
 class Orchestrator():
 
-    def __init__(self):
+    def __init__(self, orchestratorStatus: OrchestratorStatus):
         self.orchestratorID = uuid.uuid4()
         self.bufferOrder = []
+        self.status = orchestratorStatus
         print("New orchestrator initiated (" + str(self.orchestratorID) + ")")
+
+    def runOrchestation(self):
+        while True:
+            time.sleep(20)
+            print("Orchestration")
+
 
     def addNewOrder(self, phone: Phone):
         print("New Phone Added to Orch")
@@ -20,6 +30,8 @@ class Orchestrator():
 
     def penSelectedEndEvent(self):
         print("End Event Change Pen")
+        self.status.changeColor(StatusCode.IDLE)
 
     def penSelectedStartEvent(self):
         print("Start Change Pen")
+        self.status.changeColor(StatusCode.WORKING)
