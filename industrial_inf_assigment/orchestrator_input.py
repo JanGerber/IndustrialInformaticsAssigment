23c7beccae4aa0294b8195dd64a45164af69ee74
Jan Gerber <jan-456@t-online.de>
2019-11-14 23:53:11 +0100

    Menu navigation for selecting the telephone changed

--- industrial_inf_assigment/orchestrator_input.py
+++ industrial_inf_assigment/orchestrator_input.py
@@ -1,3 +1,5 @@
+import time
+
 import explorerhat
 
 from industrial_inf_assigment.orchestrator_rpi import Orchestrator
@@ -6,7 +8,7 @@ from industrial_inf_assigment.phone_color import PhoneColor
 from industrial_inf_assigment.phone_shape import PhoneShape
 
 
-class ButtonInput:
+class OrchestratorInput:
 
     def __init__(self, orchestrator: Orchestrator):
         self.phone = Phone(PhoneShape.FRAME_1, PhoneColor.RED, PhoneShape.KEYBOARD_1, PhoneColor.RED,
@@ -14,15 +16,36 @@ class ButtonInput:
         self.state = 0
         self.selected = False
         self.orchestrator = orchestrator
-        print("New Button Input Service initiated")
+        self.okButton = 1
+        print("New Orchestrator Input Service initiated")
+
+    def startListening(self):
+        explorerhat.touch.pressed(self.changeState)
+        print("Start Listening to Button Inputs")
+        while True:
+            if self.state == 0:
+                explorerhat.light[0].off()
+                time.sleep(0.1)
+            elif self.state == 7:
+                explorerhat.light[0].on()
+                time.sleep(0.2)
+                explorerhat.light[0].off()
+                time.sleep(0.2)
+            elif self.selected:
+                explorerhat.light[0].on()
+                time.sleep(0.1)
+            else:
+                explorerhat.light[0].on()
+                time.sleep(0.6)
+                explorerhat.light[0].off()
+                time.sleep(0.6)
+
 
     def changeState(self, channel, event):
-        print("Button pressed")
-        print(channel)
-        print(event)
+        print("Button pressed: " + channel)
         if self.state == 0:
             print("State 0")
-            if channel == 4:
+            if channel == self.okButton:
                 self.state = 1
         elif self.state == 1:
             self.state1(channel)
@@ -37,138 +60,99 @@ class ButtonInput:
         elif self.state == 6:
             self.state6(channel)
         elif self.state == 7:
-            if channel == 4:
+            if channel == self.okButton:
                 self.orchestrator.addNewOrder(self.phone)
-                self.resetLight()
                 self.state = 0
 
     def state1(self, channel):
-        print("State 1")
-        if channel == 4 and self.selected:
+        print("Select Frame Shape")
+        if channel == self.okButton and self.selected:
             self.state = 2
-            self.resetLight()
             self.selected = False
-        elif channel == 1:
-            self.phone.frameShape = PhoneShape.FRAME_1
-            self.changeLight(1)
-            self.selected = True
         elif channel == 2:
-            self.phone.frameShape = PhoneShape.FRAME_2
-            self.changeLight(2)
+            self.phone.frameShape = PhoneShape.FRAME_1
             self.selected = True
         elif channel == 3:
+            self.phone.frameShape = PhoneShape.FRAME_2
+            self.selected = True
+        elif channel == 4:
             self.phone.frameShape = PhoneShape.FRAME_3
-            self.changeLight(3)
             self.selected = True
 
     def state2(self, channel):
-        print("State 2")
-        if channel == 4 and self.selected:
+        print("Select Frame Color")
+        if channel == self.okButton and self.selected:
             self.state = 3
-            self.resetLight()
             self.selected = False
-        elif channel == 1:
-            self.phone.frameColor = PhoneColor.RED
-            self.changeLight(1)
-            self.selected = True
         elif channel == 2:
-            self.phone.frameColor = PhoneColor.GREEN
-            self.changeLight(2)
+            self.phone.frameColor = PhoneColor.RED
             self.selected = True
         elif channel == 3:
+            self.phone.frameColor = PhoneColor.GREEN
+            self.selected = True
+        elif channel == 4:
             self.phone.frameColor = PhoneColor.BLUE
-            self.changeLight(3)
             self.selected = True
 
     def state3(self, channel):
-        print("State 3")
-        if channel == 4 and self.selected:
+        print("Select Screen Shape")
+        if channel == self.okButton and self.selected:
             self.state = 4
-            self.resetLight()
             self.selected = False
-        elif channel == 1:
-            self.phone.screenShape = PhoneShape.SCREEN_1
-            self.changeLight(1)
-            self.selected = True
         elif channel == 2:
-            self.phone.screenShape = PhoneShape.SCREEN_2
-            self.changeLight(2)
+            self.phone.screenShape = PhoneShape.SCREEN_1
             self.selected = True
         elif channel == 3:
+            self.phone.screenShape = PhoneShape.SCREEN_2
+            self.selected = True
+        elif channel == 4:
             self.phone.screenShape = PhoneShape.SCREEN_3
-            self.changeLight(3)
             self.selected = True
 
     def state4(self, channel):
-        print("State 4")
-        if channel == 4 and self.selected:
+        print("Select Screen Color")
+        if channel == self.okButton and self.selected:
             self.state = 5
-            self.resetLight()
             self.selected = False
-        elif channel == 1:
-            self.phone.screenColor = PhoneColor.RED
-            self.changeLight(1)
-            self.selected = True
         elif channel == 2:
-            self.phone.screenColor = PhoneColor.GREEN
-            self.changeLight(2)
+            self.phone.screenColor = PhoneColor.RED
             self.selected = True
         elif channel == 3:
+            self.phone.screenColor = PhoneColor.GREEN
+            self.selected = True
+        elif channel == 4:
             self.phone.screenColor = PhoneColor.BLUE
-            self.changeLight(3)
             self.selected = True
 
     def state5(self, channel):
-        print("State 5")
-        if channel == 4 and self.selected:
+        print("Select Keyboard Shape")
+        if channel == self.okButton and self.selected:
             self.state = 6
-            self.resetLight()
             self.selected = False
-        elif channel == 1:
-            self.phone.keyboardShape = PhoneShape.KEYBOARD_1
-            self.changeLight(1)
-            self.selected = True
         elif channel == 2:
-            self.phone.keyboardShape = PhoneShape.KEYBOARD_2
-            self.changeLight(2)
+            self.phone.keyboardShape = PhoneShape.KEYBOARD_1
             self.selected = True
         elif channel == 3:
+            self.phone.keyboardShape = PhoneShape.KEYBOARD_2
+            self.selected = True
+        elif channel == 4:
             self.phone.keyboardShape = PhoneShape.KEYBOARD_3
-            self.changeLight(3)
             self.selected = True
 
     def state6(self, channel):
-        print("State 6")
-        if channel == 4 and self.selected:
+        print("Select Keyboard Color")
+        if channel == self.okButton and self.selected:
             self.state = 7
-            self.allLights()
             self.selected = False
-        elif channel == 1:
-            self.phone.keyboardColor = PhoneColor.RED
-            self.changeLight(1)
-            self.selected = True
         elif channel == 2:
-            self.phone.keyboardColor = PhoneColor.GREEN
-            self.changeLight(2)
+            self.phone.keyboardColor = PhoneColor.RED
             self.selected = True
         elif channel == 3:
+            self.phone.keyboardColor = PhoneColor.GREEN
+            self.selected = True
+        elif channel == 4:
             self.phone.keyboardColor = PhoneColor.BLUE
-            self.changeLight(3)
             self.selected = True
 
     def resetPhone(self):
         self.state = 0
-
-    def changeLight(self, param: int):
-        self.resetLight()
-        explorerhat.light[param - 1].on()
-
-    def resetLight(self):
-        explorerhat.light[0].off()
-        explorerhat.light[1].off()
-        explorerhat.light[2].off()
-
-    def allLights(self):
-        explorerhat.light[0].on()
-        explorerhat.light[1].on()
-        explorerhat.light[2].on()
